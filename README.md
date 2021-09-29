ff9bbcb07ad4759af4a94992c43c355c12e2a597
Irene <irene.ndindabahizi@gmail.com>
2021-09-29 11:13:45 +0200

    add changed files

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ErrorBudget
 
-[![Build Status](https://travis-ci.com/YayeIrene/ErrorBudget.jl.svg?branch=master)](https://travis-ci.com/YayeIrene/ErrorBudget.jl)
-[![Build Status](https://ci.appveyor.com/api/projects/status/github/YayeIrene/ErrorBudget.jl?svg=true)](https://ci.appveyor.com/project/YayeIrene/ErrorBudget-jl)
-[![Coverage](https://codecov.io/gh/YayeIrene/ErrorBudget.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/YayeIrene/ErrorBudget.jl)
-[![Coverage](https://coveralls.io/repos/github/YayeIrene/ErrorBudget.jl/badge.svg?branch=master)](https://coveralls.io/github/YayeIrene/ErrorBudget.jl?branch=master)
+[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://YayeIrene.github.io/ErrorBudget.jl/stable)
+[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://YayeIrene.github.io/ErrorBudget.jl/dev)
+[![Build Status](https://github.com/YayeIrene/WeaponSystems.jl/workflows/CI/badge.svg)](https://github.com/YayeIrene/ErrorBudget.jl/actions)
+[![Coverage](https://codecov.io/gh/YayeIrene/WeaponSystems.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/YayeIrene/ErrorBudget.jl)
--- src/types.jl
+++ src/types.jl
@@ -1,3 +1,12 @@
+"""
+    AimingError(point_def,human,res,opt_path)
+
+defines an objet 'AimingError' with fields containing the aiming error sources:
+* point_def: error (standard deviatin) on the aimpoint definition expressed as a tupple (horizontal and vertical).
+* human: human error (standard deviatin) on the aimpoint definition expressed as a tupple (horizontal and vertical).
+* res: resolution error (standard deviatin) on the aimpoint definition expressed as a tupple (horizontal and vertical).
+* opt_path: optical path bending error (standard deviatin) on the aimpoint definition expressed as a tupple (horizontal and vertical).
+"""
 mutable struct AimingError
     point_def::Tuple{Float64, Float64}
     human::Tuple{Float64, Float64}
@@ -5,6 +14,16 @@ mutable struct AimingError
     opt_path::Tuple{Float64, Float64}
 end
 
+"""
+    AlignmentError(boresight_acc,boresight_ret,sync,stab, gun_pos)
+
+defines an objet 'AlignmentError' with fields containing the alignment error sources:
+* boresight_acc: boresight accuracy (standard deviatin) expressed as a tupple (horizontal and vertical).
+* boresight_ret: boresight retention (standard deviatin) expressed as a tupple (horizontal and vertical).
+* sync: synchronism/parallelism error (standard deviatin) expressed as a tupple (horizontal and vertical).
+* stab: stabilisation error (standard deviation) expressed as a tupple (horizontal and vertical).
+* gun_pos: gun position measurement error (standard deviation) expressed as a tupple (horizontal and vertical).
+"""
 mutable struct AlignmentError
     boresight_acc::Tuple{Float64, Float64}
     boresight_ret::Tuple{Float64, Float64}
@@ -13,6 +32,22 @@ mutable struct AlignmentError
     gun_pos::Tuple{Float64, Float64}
 end
 
+"""
+    LofError(fcs,lead,cant,earth_rate, muzzle_vel, temp, press, cross_wind, range_wind, range, jump, zeroing)
+
+defines an objet 'LofError' with fields containing the line of fire error sources:
+* fcs: fire control error (standard deviation) expressed as a tupple (horizontal and vertical).
+* lead: lead angle (standard deviation) expressed as a tupple (horizontal and vertical).
+* cant: cant angle error (standard deviation) expressed as a tupple (horizontal and vertical).
+* earth_rate: earth rate error (standard deviation) expressed as a tupple (horizontal and vertical).
+* muzzle_vel: muzzle velocity error (standard deviation) expressed as a tupple (horizontal and vertical).
+* temp: temperature error (standard deviation) expressed as a tupple (horizontal and vertical).
+* press: pressure error (standard deviation) expressed as a tupple (horizontal and vertical).
+* cross_wind: cross wind error (standard deviation) expressed as a tupple (horizontal and vertical).
+* range_wind: range wind error (standard deviation) expressed as a tupple (horizontal and vertical).
+* jump: fleet jump error (standard deviation) expressed as a tupple (horizontal and vertical).
+* zeroing: calibration error (standard deviation) expressed as a tupple (horizontal and vertical).
+"""
 mutable struct LofError
     fcs::Tuple{Float64, Float64}
     lead::Tuple{Float64, Float64}
@@ -28,10 +63,26 @@ mutable struct LofError
     zeroing::Tuple{Float64, Float64}
 end
 
+"""
+    FlightError(aero)
+
+defines an objet 'FlightError' which contains the projectile dispersion as
+an error (standard deviation) expressed as a tupple (horizontal and vertical).
+"""
 mutable struct FlightError
     aero::Tuple{Float64, Float64}
 end
 
+"""
+    UnitEffect(velocity,temp,density, windR,windC)
+
+Returns the UnitEffects for different ranges. The parameters considered are:
+    * velocity
+    * temp: temperature
+    * density
+    * windR: wind range
+    * windC: cross wind
+"""
 mutable struct UnitEffect
     velocity::Vector{Float64}
     temp::Vector{Float64}
@@ -40,6 +91,11 @@ mutable struct UnitEffect
     windC::Vector{Float64}
 end
 
+"""
+    Error(horizontal, vertical)
+
+Returns the Error object with field: 'horizontal' and 'vertical'
+"""
 mutable struct Error
     horizontal::Float64
     vertical::Float64
