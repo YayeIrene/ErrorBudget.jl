a84b83a9c664e99abe44a9c3d6914d824c5443df
Irene <irene.ndindabahizi@gmail.com>
2021-10-06 11:38:22 +0200

    bug fix

--- src/ErrorBudget.jl
+++ src/ErrorBudget.jl
@@ -7,7 +7,7 @@ using Interpolations
 
 # Write your package code here.
 include("types.jl")
-include("transformations.jl")
+#include("transformations.jl")
 include("paralax.jl")
 include("variableBias.jl")
 include("totalError.jl")
@@ -16,6 +16,6 @@ include("generate.jl")
 
 export μ, AimingError, AlignmentError, LofError, FlightError, σ, UnitEffect, windCrossError,windRangeError,interTable,rangeError,
         elevationError, azimuthError, muzzleVelError, temperatureError, cantError, Error, variableBias, TargetRect, TargetCirc,
-        SpheError, createTargetRect, createTargetCirc, targetPos, muzzlePos, muzzleVel, wind
+        SpheError, createTargetRect, createTargetCirc
 
 end
